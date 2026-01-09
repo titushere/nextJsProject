3fbe873dfb9e835a6c0b706947cc87c4a71a4c7e
Titus Chacko <107310515+titushere@users.noreply.github.com>
2026-01-10 00:24:29 +0530

    fix bugs

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -2,12 +2,14 @@
 
 import {z} from 'zod';
 import postgres from 'postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customer_id: z.string(),
+    customerId: z.string(),
     amount: z.coerce.number(),
     status: z.enum(['pending','paid']),
     date: z.string(),
@@ -22,7 +24,7 @@ export async function createInvoice(formData:FormData){
         status: formData.get('status'),
     });
 
-    const amountInCents = amount * 1.00;
+    const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
     // console.log('rawFormData',rawFormData);
@@ -31,4 +33,7 @@ export async function createInvoice(formData:FormData){
         INSERT INTO invoices (customer_id, amount, status, date) 
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
